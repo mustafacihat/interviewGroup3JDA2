c821e2a4a8b6a9dcb95ef7a7671e1705c7252e8b
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-04-03 17:43:26 +0200

    added GenerateDocumentBruteForceSolution.java and GenerateDocumentOptimalSolution

--- src/a1generateDocument/cydeo/GenerateDocumentBruteForceSolution.java
+++ src/a1generateDocument/cydeo/GenerateDocumentBruteForceSolution.java
@@ -1,5 +1,36 @@
 package a1generateDocument.cydeo;
 
+
 public class GenerateDocumentBruteForceSolution {
+
+    public static void main(String[] args) {
+        String characters = "abcccdd";
+        String document = "abc";
+
+        System.out.println((generateDocumentBruteForce(characters, document)));
+    }
+
+    public static boolean generateDocumentBruteForce(String ch, String doc) {
+        for (int i = 0; i < doc.length(); i++) {
+            char character = doc.charAt(i);
+            int documentFrequency = countCharacterFrequency(character, doc);
+            int charactersFrequency = countCharacterFrequency(character, ch);
+            if (documentFrequency > charactersFrequency) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countCharacterFrequency(char character, String target) {
+        int frequency = 0;
+        for (int i = 0; i < target.length(); i++) {
+            char c = target.charAt(i);
+            if (c == character) {
+                frequency += 1;
+            }
+        }
+        return frequency;
+    }
 
 }
--- src/a1generateDocument/cydeo/GenerateDocumentOptimalSolution.java
+++ src/a1generateDocument/cydeo/GenerateDocumentOptimalSolution.java
@@ -1,7 +1,55 @@
 package a1generateDocument.cydeo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class GenerateDocumentOptimalSolution {
+
+    public static void main(String[] args) {
+        String characters = "veDJaCyd vaeo perelo xw!xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        String document = "Cydeo Java Developer!";
+
+        long start = System.currentTimeMillis();
+        System.out.println((generateDocumentOptimal(characters, document)));
+        long end = System.currentTimeMillis();
+        System.out.println("execution time : " + (end-start) + " ms");
+    }
+
+    public static boolean generateDocumentOptimal(String characters, String document) {
+
+        // O(n + m) time | O(c) space - where n is the number of characters, m is
+        // the length of the document, and c is the number of unique characters in the
+        // characters string
+
+        if (document.equals("")) return true;
+        if (characters.length() < document.length()) return false;
+
+
+        HashMap<Character, Integer> characterCounts = new HashMap<Character, Integer>();
+        for (int i = 0; i < characters.length(); i++) {
+            char character = characters.charAt(i);
+
+//            characterCounts.putIfAbsent(character,0);
+//            characterCounts.put(character,characterCounts.get(character)+1);
+//            if (characterCounts.containsKey(character)) {
+//                characterCounts.put(character, characterCounts.get(character) + 1);
+//            } else {
+//                characterCounts.put(character, 1);
+//            }
+            characterCounts.put(character, characterCounts.getOrDefault(character, 0) + 1);
+        }
+        for (int i = 0; i < document.length(); i++) {
+            char character = document.charAt(i);
+            if (!characterCounts.containsKey(character) || characterCounts.get(character) == 0){
+                return false;
+            }
+            characterCounts.put(character, characterCounts.get(character) - 1);
+        }
+        return true;
+
+
+    }
 
 }
