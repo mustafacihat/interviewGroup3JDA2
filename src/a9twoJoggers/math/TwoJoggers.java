2c576d0e35321def9ec121ac17fc219b35282a3c
Mehmet <mathdenizi@gmail.com>
2022-06-11 23:01:20 +0200

    Two Jogger  with Stream

--- src/a9twoJoggers/math/TwoJoggers.java
+++ src/a9twoJoggers/math/TwoJoggers.java
@@ -2,13 +2,28 @@ package a9twoJoggers.math;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
-
+import java.util.stream.IntStream;
 
 
 public class TwoJoggers {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(nbrOfLaps(5, 3)));
-        System.out.println(Arrays.toString(nbrOfLaps(4, 6)));
+
+        int a = 6;
+        int b = 4;
+
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        int lcm = IntStream.rangeClosed(1,min)
+                .filter(num -> (num * max) % min == 0)
+                .map(num -> Math.abs(num * max))
+                .findFirst()
+                .getAsInt();
+
+        System.out.println(lcm/a + " , " + lcm/b);
+        System.out.println(Arrays.toString(nbrOfLaps(a,b)));
+
+
     }
 
 
@@ -19,9 +34,10 @@ public class TwoJoggers {
 
         int higher = Math.max(a,b);
         int lower = Math.min(a,b);
-        //find least common multiple first
+        // find lowest com multiple
         int lcm = higher;
         while (lcm % lower != 0) {
+
             lcm += higher;
         }
         return new int[] {lcm/a , lcm/b} ;
